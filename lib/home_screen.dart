b2dc608176616348917767dd268673860f1dd189
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-02-16 13:55:23 +0200

    finish requset implementation

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,6 +13,36 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(
+            child: TextButton(
+                onPressed: () {
+                  selectFile();
+                },
+                child: const Text('upload file')),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+selectFile() async {
+  FilePickerResult? file = await FilePicker.platform.pickFiles();
+  if (file?.files.single.path == null) return;
+  uploadFile(File(file!.files.single.path!));
+}
+
+uploadFile(File file) async {
+  var multipartRequest = http.MultipartRequest('POST', Uri.parse('url'));
+  var length = await file.length();
+  var stream = http.ByteStream(file.openRead());
+  var multipartFile =
+      http.MultipartFile('name', stream, length, filename: basename(file.path));
+  multipartRequest.files.add(multipartFile);
+  var response = await multipartRequest.send();
+  if (response.statusCode == 200) {
+    // TODO
   }
 }
